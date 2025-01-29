cae2ee646bd8003329e5b25328d05e20d301aa29
Eileen909 <eileenmthayil@gmail.com>
2025-01-29 23:17:44 +0530

    Eil-New Commit

--- src/App.js
+++ src/App.js
@@ -1,41 +1,35 @@
 import React, {useState} from 'react';
 import './App.css'; 
-// import bottonImage from './assets/wave.jpg';
 
 const App = () => { 
   const[modelOpen, setModelOpen]=useState(false);
-  const[clickedBox, setClickedBox]=useState("");
+  const[clickedBox, setClickedBox]=useState("");  
   const handleClickedBox=(boxId)=>{
     console.log(`onClick invoked!-${boxId}`);
     setClickedBox(boxId);
     setModelOpen(true);
   };
-  // const modelClose=()=>{
-  //   setModelOpen(false);
-  //   setClickedBox("");
-  // };
+  const modelClose=()=>{
+    setModelOpen(false);
+    setClickedBox("");
+  };
 
   return (
     <div className="app-container">
       <h1>breath smart</h1>
       <div className="container">
         <button className="box" id="box1" onClick={()=> handleClickedBox("Box1 Clicked")}>All Types</button>
-        <button className="box" id="box3" onClick={()=> handleClickedBox("Box2 Clicked")}>Box Breathing</button>
-        <button className="box" id="box4" onClick={()=> handleClickedBox("Box3 Clicked")}>Tumo Breathing</button>
-        <button className="box" id="box5" onClick={()=> handleClickedBox("Box4 Clicked")}>Focus</button>
-        <button className="box" id="box6" onClick={()=> handleClickedBox("Box5 Clicked")}>Sleep</button>
-        <button className="box" id="box2" onClick={()=> handleClickedBox("Box6 Clicked")}>Wim-Hof</button>
+        <button className="box" id="box2" onClick={()=> handleClickedBox("Box2 Clicked")}>Wim-Hof</button>
+        <button className="box" id="box3" onClick={()=> handleClickedBox("Box3 Clicked")}>Box Breathing</button>
+        <button className="box" id="box4" onClick={()=> handleClickedBox("Box4 Clicked")}>Tumo Breathing</button>
+        <button className="box" id="box5" onClick={()=> handleClickedBox("Box5 Clicked")}>Focus</button>
+        <button className="box" id="box6" onClick={()=> handleClickedBox("Box6 Clicked")}>Sleep</button>
         <button className="box" id="box7" onClick={()=> handleClickedBox("Box7 Clicked")}>Calm</button>
       </div>
+
       <p>Choose your breathing style</p>
-      {/* <div className="bottom-image">
-        <img src={bottonImage} alt="Wave"></img>
-      </div> */}
-
-
     </div>
   );
 };
 
 export default App;
-
